5d96e4b7aec450b67ed345ab48e1267c55135e0a
RatnaPriya-2 <ratnapriya.t@gmail.com>
2025-06-12 07:09:09 -0400

    authRoutes

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -1,17 +1,24 @@
 const express = require('express');
-const router = express.Router();
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const logger = require('../logger');
 const connectToDatabase = require('../models/db');
+const router = express.Router();
+const dotenv = require('dotenv');
+const pino = require('pino');  // Import Pino logger
+dotenv.config();
+
+const logger = pino();  // Create a Pino logger instance
+
+//Create JWT secret
 const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/register', async (req, res) => {
     try {
-        const db = await connectToDatabase();
-        const collection = db.collection("users");
+      //Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+      const db = await connectToDatabase();
+      const collection = db.collection("users");
+      const existingEmail = await collection.findOne({ email: req.body.email });
 
-        const existingEmail = await collection.findOne({ email: req.body.email });
         if (existingEmail) {
             logger.error('Email id already exists');
             return res.status(400).json({ error: 'Email id already exists' });
@@ -20,7 +27,6 @@ router.post('/register', async (req, res) => {
         const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
         const email=req.body.email;
-
         const newUser = await collection.insertOne({
             email: req.body.email,
             firstName: req.body.firstName,
@@ -37,10 +43,9 @@ router.post('/register', async (req, res) => {
 
         const authtoken = jwt.sign(payload, JWT_SECRET);
         logger.info('User registered successfully');
-        res.json({ authtoken, email });
-
+        res.json({ authtoken,email });
     } catch (e) {
-        logger.error('Registration error:', e);
+        logger.error(e);
         return res.status(500).send('Internal server error');
     }
 });
